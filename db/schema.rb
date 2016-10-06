1d34cfdf635408debabf64124f331d67e63df4e5
alxh87 <alexmhorn@gmail.com>
2016-10-06 22:47:09 +0800

    gmap added to new user page, not yet adding params to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20161006081136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "authentications", force: :cascade do |t|
-    t.string   "uid"
-    t.string   "provider"
-    t.string   "token"
-    t.integer  "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                       default: "",    null: false
@@ -41,13 +31,16 @@ ActiveRecord::Schema.define(version: 20161006081136) do
     t.datetime "updated_at",                                  null: false
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "state"
     t.string   "country"
-
-    t.boolean  "admin",                  default: false
+    t.boolean  "admin",                       default: false
+    t.string   "location"
+    t.string   "formatted_address"
+    t.string   "route"
+    t.string   "postal_code"
+    t.string   "locality"
+    t.string   "administrative_area_level_1"
     t.string   "provider"
     t.string   "uid"
-
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
