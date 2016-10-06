9d0178a2760281f7c39d53f8eca5e7dc994fee46
alxh87 <alexmhorn@gmail.com>
2016-10-06 18:06:23 +0800

    about to pull master, form kinda working

--- db/schema.rb
+++ db/schema.rb
@@ -11,30 +11,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161006043022) do
+ActiveRecord::Schema.define(version: 20161006073715) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.string   "email",                       default: "",    null: false
+    t.string   "encrypted_password",          default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",               default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
+    t.datetime "created_at",                                  null: false
+    t.datetime "updated_at",                                  null: false
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "address"
     t.string   "state"
     t.string   "country"
-    t.boolean  "admin",                  default: false
+    t.boolean  "admin",                       default: false
+    t.string   "location"
+    t.string   "formatted_address"
+    t.string   "route"
+    t.string   "postal_code"
+    t.string   "locality"
+    t.string   "administrative_area_level_1"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
