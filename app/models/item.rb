3d1c2304a9c4fafebee7d20c42bcf4d8abd963b3
Dandibot <rakotoniaina.andry@gmail.com>
2016-10-07 14:31:57 +0800

    Add static map to index page

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,43 @@
 class Item < ActiveRecord::Base
 	belongs_to :user
+	has_many :rentals
 
+	def index
+		@item = Items.all 
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+		item = Item.create(item_params)
+	end
+
+	def edit
+
+	end
+
+	def update
+		
+	end
+
+	def destroy
+
+	end		
 	
+private
+
+	def item_params
+
+		params.require(:item).permit(:name, :description, :user_id, {avatars[]})
+
+	end
 end
+
+name
+description
+avatars
+user_id
+
+
