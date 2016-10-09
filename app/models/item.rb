f72b386b71419e4fca391fd5c6b8916b3d96d166
alxh87 <alexmhorn@gmail.com>
2016-10-09 16:29:20 +0800

    cards working even without images

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,12 @@ class Item < ActiveRecord::Base
 	acts_as_taggable # Alias for acts_as_taggable_on :tags
 
 	def search_card
-		avatar=self.avatar.medium.url
-		return '<a href="/items/'+self.id.to_s+'">'+self.title+' $'+self.price_per_day.to_s+'<img style="max-width: 50px; max-height: 50px"src='+avatar+'></a><br>'+self.description
+		if self.avatar.medium.url
+			avatar=self.avatar.medium.url
+			return '<a href="/items/'+self.id.to_s+'">'+self.title+' $'+self.price_per_day.to_s+'<img style="max-width: 50px; max-height: 50px"src='+avatar+'></a><br>'+self.description
+		else
+			return '<a href="/items/'+self.id.to_s+'">'+self.title+' $'+self.price_per_day.to_s+'</a><br>'+self.description
+		end
 	end
 
 end
