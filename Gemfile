df38e274b94680f7012575a0d9d168f7154f5585
Dandibot <rakotoniaina.andry@gmail.com>
2016-10-11 10:29:30 +0800

    Go to seed

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'devise', '~> 4.1', '>= 4.1.1'
 
 gem 'searchkick'
 
+gem 'random-location'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,15 +8,27 @@
 require 'random-location'
 require 'faker'
 
+items = ["","sous vide", "sander", "projector", "PS4", "projector", "lawnmower", "toolbox", "fitness bench", "golf clubs", "fitness bicycle", "dumbbells", "treadmill", "diving suit", "skiing suit", "projector", "vacuum cleaner", "toolbox", "tool box", "drill", "mix table", "george foreman grill", "caravan", "trailer", "trailer", "mountain bike", "motorbike", "mountain bike", "cooking machine", "bread maker", "scuba diving equipment"]
+
 # User
 for u in (1..30)
-	User.create(:name => Faker::Name.name, :email => Faker::Internet.free_email, :password => "1234", :address => RandomLocation.near_by(3.139474, 101.636964, 10000), :avatars => ["image-#{u}"], :admin => false)
+	User.create(:name => Faker::Name.name, :email => Faker::Internet.free_email, :password => "123456", :address => RandomLocation.near_by(3.139474, 101.636964, 10000), :avatars => ["image-#{u}"], :admin => false)
 end
 
 # Item
 for i in (1..30)
-	Item.create(:title => Faker::Hipster.sentence(3), :description => Faker::Hipster.paragraph, :avatars => ["item#{i}"], :price_per_day => (1..10).sample, :user_id => i)
+	Item.create(:title => Faker::Hipster.words(2).flatten, :description => Faker::Hipster.sentences(1)[0], :avatars => ["item#{i}"], :price_per_day => (1..10).sample, :user_id => i)
 end
+
+    t.string   "title",         null: false
+    t.string   "description",   null: false
+    t.integer  "price_per_day", null: false
+    t.integer  "user_id",       null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "avatar"
+
+
 
 # Rental
 
