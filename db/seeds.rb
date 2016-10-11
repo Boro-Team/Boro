242524bec16ccc7e9568119e069e92d540cfe18e
Dandibot <rakotoniaina.andry@gmail.com>
2016-10-11 10:43:03 +0800

    Seed User

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,26 +9,22 @@ require 'random-location'
 require 'faker'
 
 items = ["","sous vide", "sander", "projector", "PS4", "projector", "lawnmower", "toolbox", "fitness bench", "golf clubs", "fitness bicycle", "dumbbells", "treadmill", "diving suit", "skiing suit", "projector", "vacuum cleaner", "toolbox", "tool box", "drill", "mix table", "george foreman grill", "caravan", "trailer", "trailer", "mountain bike", "motorbike", "mountain bike", "cooking machine", "bread maker", "scuba diving equipment"]
+firstname = Faker::Name.first_name
+lastname  = Faker::Name.last_name
+name 			= firstname + " " + lastname 
+email 		= firstname + "." + lastname + "@boro.com"
+latitude 	= RandomLocation.near_by(3.134857, 101.629915, 10000).latitude
+longitude = RandomLocation.near_by(3.134857, 101.629915, 10000).longitude
 
 # User
 for u in (1..30)
-	User.create(:name => Faker::Name.name, :email => Faker::Internet.free_email, :password => "123456", :address => RandomLocation.near_by(3.139474, 101.636964, 10000), :avatars => ["image-#{u}"], :admin => false)
+	User.create(:name => Faker::Name.name, :email => Faker::Internet.free_email, :password => "123456", :latitude => latitude, :longitude => longitude, :avatars => ["image-#{u}"], :admin => false)
 end
 
 # Item
 for i in (1..30)
-	Item.create(:title => Faker::Hipster.words(2).flatten, :description => Faker::Hipster.sentences(1)[0], :avatars => ["item#{i}"], :price_per_day => (1..10).sample, :user_id => i)
+	Item.create(:title => items[i], :description => Faker::Hipster.sentences(1)[0], :avatars => ["item#{i}"], :price_per_day => (1..10).sample, :user_id => i)
 end
-
-    t.string   "title",         null: false
-    t.string   "description",   null: false
-    t.integer  "price_per_day", null: false
-    t.integer  "user_id",       null: false
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.string   "avatar"
-
-
 
 # Rental
 
