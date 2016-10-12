307f023b4883f349a098a6913438ba5bd2403480
Dandibot <rakotoniaina.andry@gmail.com>
2016-10-12 15:36:24 +0800

    Back in time

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 	
 	def index
-    # @users = User.all
+    @users = User.all
     
     if params[:formatted_address] == "" or params[:formatted_address]==nil
       @users=User.all
